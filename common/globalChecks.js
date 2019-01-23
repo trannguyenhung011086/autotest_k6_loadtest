a38c35dae7a39b60a2b00d6e61e316d6f37bfcc0
Hung Tran <hungtn@leflair.vn>
2019-01-23 10:48:08 +0700

    change globalcheck

--- common/globalChecks.js
+++ common/globalChecks.js
@@ -3,6 +3,6 @@ import { check } from 'k6'
 export default function (res, duration, status = 200) {
     return check(res, {
         'status is correct': r => r.status == status,
-        'transaction time is less than threshold': r => r.timings.duration < duration
+        // 'transaction time is less than threshold': r => r.timings.duration < duration
     })
 }
